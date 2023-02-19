36b1df0373d19765d7dd454ebc5a35a479f341e5
zhanyarios004gardi004 <119080464+zhanyarios004gardi004@users.noreply.github.com>
2023-02-19 23:00:05 +0300

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,43 @@
+var routes = [
+   {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/allapps/',
+    url: './pages/allapps.html',
+  },
+  {
+    path: '/jilbreak/',
+    url: './pages/jb.html',
+  },
+  {
+    path: '/pubgmobile/',
+    url: './pages/pubg.html',
+  },
+  {
+    path: '/allgame/',
+    url: './pages/game.html',
+  },
+  {
+    path: '/socialmedia/',
+    url: './pages/socialm.html',
+  },
+  {
+    path: '/credits/',
+    url: './credits.html',
+  },
+  {
+    path: '/apps/',
+    url: './apps.html',
+  },
+  {
+    path: '/updates/',
+    url: './updates.html',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
